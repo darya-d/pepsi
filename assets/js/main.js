35d3485592062f2dc88ad44e910fdbe8c5c2ceaf
Darya <darya.drazdouskaya@gmail.com>
2020-11-22 22:02:22 +0300

    add adaptive version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
-function imgSlider(anything){
+function imgSlider(anything) {
   document.querySelector('.pepsi').src = anything;
 }
-function changeColor(color){
-  const bgColor = document.querySelector('body');
+function changeColor(color) {
+  const bgColor = document.querySelector('section');
   bgColor.style.background = color;
+}
+
+function openToggleMenu() {
+  const toggleMenu = document.querySelector('.toggleMenu');
+  const nav = document.querySelector('.header__nav');
+  toggleMenu.classList.toggle('active');
+  nav.classList.toggle('active');
 }
