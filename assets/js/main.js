bc0c090f42d9ddfd938933554c030be94c7ac06d
Darya <darya.drazdouskaya@gmail.com>
2020-11-22 20:29:58 +0300

    add project

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+  document.querySelector('.pepsi').src = anything;
+}
+function changeColor(color){
+  const bgColor = document.querySelector('body');
+  bgColor.style.background = color;
+}
